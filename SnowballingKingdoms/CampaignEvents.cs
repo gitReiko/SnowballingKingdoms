92626bbb04cafce1b1b951e2f9f5ccfba6a50182
Denis Makouski (Reiko) <khornau@gmail.com>
2024-12-06 19:37:13 +0300

    Add project files.

---
+++ SnowballingKingdoms/CampaignEvents.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.Library;
+
+namespace SnowballingKingdoms
+{
+    internal class SnowballEvents : CampaignBehaviorBase
+    {
+
+        public override void SyncData(IDataStore dataStore) { }
+
+        public override void RegisterEvents()
+        {
+            CampaignEvents.DailyTickEvent.AddNonSerializedListener(this, DailyTickEvent);
+        }
+
+        private void DailyTickEvent()
+        {
+            InformationManager.DisplayMessage(new InformationMessage("message 123", TaleWorlds.Library.Color.ConvertStringToColor("#FF0042FF")));
+        }
+
+    }
+}
---
+++ SnowballingKingdoms/MySubModule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.LinQuick;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace SnowballingKingdoms
+{
+    public class MySubModule : MBSubModuleBase 
+    {
+
+        protected void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+        }
+
+        public override void OnCampaignStart(Game game, object starterObject)
+        {
+            this.InitializeGame(game, (IGameStarter)starterObject);
+        }
+
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            this.InitializeGame(game, gameStarterObject);
+        }
+
+        public void InitializeGame(Game game, IGameStarter gameStarter)
+        {
+            this.AddBehaviours(gameStarter as CampaignGameStarter);
+        }
+
+        private void AddBehaviours(CampaignGameStarter starter)
+        {
+            starter.AddBehavior(new SnowballEvents());
+        }
+
+    }
+}
