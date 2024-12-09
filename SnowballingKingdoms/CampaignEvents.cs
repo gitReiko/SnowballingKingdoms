33202548436d846133da8beccf7d095301f219da
Denis Makouski (Reiko) <khornau@gmail.com>
2024-12-09 19:49:56 +0300

    kingdoms get. setllement get

--- SnowballingKingdoms/CampaignEvents.cs
+++ SnowballingKingdoms/CampaignEvents.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.MountAndBlade;
 using TaleWorlds.Library;
+using TaleWorlds.Core;
+using TaleWorlds.CampaignSystem.Settlements;
+using System.Net.NetworkInformation;
 
 namespace SnowballingKingdoms
 {
@@ -21,7 +24,52 @@ namespace SnowballingKingdoms
 
         private void DailyTickEvent()
         {
-            InformationManager.DisplayMessage(new InformationMessage("message 123", TaleWorlds.Library.Color.ConvertStringToColor("#FF0042FF")));
+            foreach(Kingdom kingdom in Kingdom.All)
+            {
+
+
+
+
+                InformationManager.DisplayMessage(new InformationMessage(kingdom.Name.Value, TaleWorlds.Library.Color.ConvertStringToColor("#FF0042FF")));
+
+                Settlement settlement = get_kingdom_settlement(kingdom);
+
+                InformationManager.DisplayMessage(new InformationMessage(settlement.Name.Value, TaleWorlds.Library.Color.ConvertStringToColor("#FF1342FF")));
+
+                break;
+            }
+            
+            
+            //InformationManager.DisplayMessage(new InformationMessage("message 123", TaleWorlds.Library.Color.ConvertStringToColor("#FF0042FF")));
+        }
+
+        private Settlement get_kingdom_settlement(Kingdom kingdom)
+        {
+            foreach (Settlement settlement in kingdom.Settlements)
+            {
+                if (settlement.IsCastle || settlement.IsTown)
+                {
+                    return settlement;
+                }
+            }
+
+            return null;
+        }
+
+
+        private void CreateNewClan()
+        {
+            //HeroCreator.CreateSpecialHero
+
+
+            //Kingdom.ge
+
+            //Settlement settlement = this.GetValidSettlement();
+
+
+
+
+
         }
 
     }
